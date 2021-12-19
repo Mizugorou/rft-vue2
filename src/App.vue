8014913764157e79431057e4ef69b1e365fb601e
Mizugorou <boss2wolf@gmail.com>
2021-12-20 00:04:05 +0700

    1

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,24 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Contain></Contain>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Contain from './components/Contain.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Contain
   }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Contain.vue
@@ -0,0 +1,63 @@
+<template>
+  <div >
+    <ef-panel id="login-page" spacing>
+      <ef-loader v-if="loading"></ef-loader>
+      <template v-else>
+        <h1>{{ title }}</h1>
+        <ef-text-field placeholder="Username"></ef-text-field>
+        <ef-password-field placeholder="Password"></ef-password-field>
+        <div id="button-group">
+          <ef-button v-on:click="login">Login</ef-button>
+          <ef-button class="test">Cancel</ef-button>
+        </div>
+      </template>
+    </ef-panel>
+  </div>
+</template>
+
+<script>
+import '@refinitiv-ui/elements/lib/loader';
+import '@refinitiv-ui/elements/lib/button';
+import '@refinitiv-ui/elements/lib/panel';
+import '@refinitiv-ui/elements/lib/text-field';
+import '@refinitiv-ui/elements/lib/password-field';
+
+export default {
+  name: 'Contain',
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      title: 'Hello!',
+      loading: false,
+    };
+  },
+  methods: {
+    login: function() {
+      this.loading = true;
+
+      setTimeout(() => {
+        this.title = 'Done!';
+        this.loading = false;
+      }, 3000);
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+#login-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 450px;
+  height: 200px;
+  margin: 40px auto;
+}
+
+#button-group {
+  margin: 10px 0;
+}
+</style>
