9531a7b34e6c6b9009841725054df3dc88ed8551
Mizugorou <boss2wolf@gmail.com>
2021-12-28 09:41:19 +0700

    vanilla

---
+++ vanilla/src/index.js
@@ -0,0 +1,16 @@
+console.log('asd')
+// Polyfills
+import '@refinitiv-ui/polyfills';
+
+// Components
+import '@refinitiv-ui/elements/lib/button';
+import '@refinitiv-ui/elements/lib/panel';
+import '@refinitiv-ui/elements/lib/text-field';
+import '@refinitiv-ui/elements/lib/password-field';
+
+// Themes
+import '@refinitiv-ui/halo-theme/dark/imports/native-elements';
+import '@refinitiv-ui/elements/lib/button/themes/halo/dark';
+import '@refinitiv-ui/elements/lib/panel/themes/halo/dark';
+import '@refinitiv-ui/elements/lib/text-field/themes/halo/dark';
+import '@refinitiv-ui/elements/lib/password-field/themes/halo/dark';
